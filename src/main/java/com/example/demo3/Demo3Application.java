8a927becde50ce78bb209af292add13656c127a1
Kundan Kumar <kundan.kumar4@ikea.com>
2018-12-04 11:24:39 +0100

    Initial commit

---
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -0,0 +1,87 @@
+package com.example.demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@SpringBootApplication
+@RestController
+public class Demo3Application {
+   private static Map<String, Product> productRepo = new HashMap<>();
+   static {
+      Product honey = new Product();
+      honey.setId("1");
+      honey.setName("Honey");
+      productRepo.put(honey.getId(), honey);
+      
+      Product almond = new Product();
+      almond.setId("2");
+      almond.setName("Almond");
+      productRepo.put(almond.getId(), almond);
+   }
+   
+  /* @Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurerAdapter() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/view-products//*").allowedOrigins("http://localhost:8080");
+         }
+      };
+   }*/
+   
+   public static void main(String[] args) {
+      SpringApplication.run(Demo3Application.class, args);
+   }
+   
+   @CrossOrigin
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+   public ResponseEntity<Object> delete(@PathVariable("id") String id) { 
+	   System.out.println("Demo3Application.delete()");
+      productRepo.remove(id);
+      return new ResponseEntity<>(HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+   public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+	   System.out.println("Demo3Application.updateProduct()");
+      productRepo.remove(id);
+      product.setId(id);
+      productRepo.put(id, product);
+      return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/products", method = RequestMethod.POST)
+   public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+	   System.out.println("Demo3Application.createProduct()");
+      productRepo.put(product.getId(), product);
+      return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+   }
+   
+   @RequestMapping(value = "/products")
+   public ResponseEntity<Object> getProduct() {
+	   System.out.println("Demo3Application.getProduct()");
+      return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+   }
+}
